7ea78d35f033fc2bf6c368a52d74554fd1714735
Puthinart-S <boatkakkakmak@gmail.com>
2021-04-30 01:33:07 +0700

    sign in and sign out ได้แล้ว

---
+++ src/components/LogIn.js
@@ -0,0 +1,55 @@
+import React, { useContext } from 'react';
+import { Redirect } from 'react-router-dom';
+import { AuthContext } from './Auth';
+import firebaseConfig from '../config';
+import { Container } from 'react-bootstrap';
+
+const LogIn = () => {
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        
+        const { email, password } = e.target.elements;
+        
+        try{
+
+            firebaseConfig.auth().signInWithEmailAndPassword(email.value, password.value);
+
+        } catch(error) {
+            alert(error);
+        }
+    }
+
+    const { currentUser } = useContext(AuthContext);
+    if (currentUser) {
+        return <Redirect to="/Testsignupandin" />
+    }
+
+    return (
+        <React.Fragment>
+            <Container className="mt-5">
+            <h1>Log In</h1>
+            <form onSubmit={handleSubmit}>
+                <div className="mb-3">
+                    <label for="exampleInputEmail1" className="form-label">Email address</label>
+                    <input type="email" name="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                </div>
+                <div className="mb-3">
+                    <label for="exampleInputPassword1" className="form-label">Password</label>
+                    <input type="password" name="password" className="form-control" id="exampleInputPassword1"/>
+                </div>
+                <div className="mb-3 form-check">
+                <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
+                <label className="form-check-label" for="exampleCheck1">Check me out</label>
+                </div>
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
+            </Container>
+        </React.Fragment>
+    )
+
+}
+
+export default LogIn;
+
---
+++ src/config.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase/app';
+import 'firebase/auth'
+
+const firebaseConfig = firebase.initializeApp({
+    apiKey: "AIzaSyCEVf6I_cXVhorJdUJGmNeP7dfAzDWNs_g",
+    authDomain: "bbiddoff.firebaseapp.com",
+    projectId: "bbiddoff",
+    storageBucket: "bbiddoff.appspot.com",
+    messagingSenderId: "754560189188",
+    appId: "1:754560189188:web:a7f62594764d246a2d8651",
+    measurementId: "G-04W33XXC55"
+  });
+
+  export default firebaseConfig;
