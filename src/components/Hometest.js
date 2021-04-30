96edfb4ab67fdcc85fbe1527e90164d802039f6b
Puthinart-S <boatkakkakmak@gmail.com>
2021-04-30 13:52:36 +0700

    update sth ให้เข้าใจง่าย

--- src/components/Hometest.js
+++ src/components/Hometest.js
@@ -3,13 +3,13 @@ import { Link } from 'react-router-dom';
 import { AuthContext } from './Auth';
 
 const TestHome = () => {
-    const { currenUser } = useContext(AuthContext);
+    const { currentUser } = useContext(AuthContext);
 
     return (
         <React.Fragment>
             <div className="container mt-5">
                 <h1>HomeTest</h1>
-                {currenUser ? (
+                {currentUser ? (
                     <p>You are logged in - <Link to="/Testsignupandin">View teststuff</Link></p>
                 ) : (
                     <p>
