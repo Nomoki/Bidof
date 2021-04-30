bc4c6ba1bb5657004fb3b7c81608b28b1f10db55
Puthinart-S <boatkakkakmak@gmail.com>
2021-04-30 14:33:05 +0700

    change func to class

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Hometest from './components/Hometest';
 import Testsignupandin from './components/Testsignupandin';
@@ -6,19 +6,21 @@ import LogIn from './components/LogIn';
 import SignUp from './components/SignUp';
 import { AuthProvider } from './components/Auth';
 
-function App() {
-  return (
-    <AuthProvider>
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Hometest}/>
-          <Route exact path="/Testsignupandin" component={Testsignupandin}/>
-          <Route exact path="/LogIn" component={LogIn}/>
-          <Route exact path="/SignUp" component={SignUp}/>
-        </Switch>
-      </Router>
-    </AuthProvider>
-  );
+class App extends Component {
+  render () {
+    return (
+      <AuthProvider>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Hometest}/>
+            <Route exact path="/Testsignupandin" component={Testsignupandin}/>
+            <Route exact path="/LogIn" component={LogIn}/>
+            <Route exact path="/SignUp" component={SignUp}/>
+          </Switch>
+        </Router>
+      </AuthProvider>
+    );
+  };
 }
 
 export default App;
