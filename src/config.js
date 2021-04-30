da9396fef069cf8109ed6e67c9a9f15d98fdbe54
Puthinart-S <boatkakkakmak@gmail.com>
2021-05-01 04:56:13 +0700

    env

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ import firebase from 'firebase/app';
 import 'firebase/auth'
 
 const firebaseConfig = firebase.initializeApp({
-  apiKey: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
