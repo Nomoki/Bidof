debfca83408b217ecbdc9c53c0b131ac67da053d
Nomoki <sroravitoak@hotmail.com>
2021-05-01 06:44:02 +0700

    navbar added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useAuth } from './AuthContext';
+import button from 'react-bootstrap/Button'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Button,Navbar,Nav } from 'react-bootstrap';
 
 const Home = () => {
     const { currentUser } = useAuth();
 
     return (
         <React.Fragment>
-            <div className="container mt-5">
+            <div className="container">
+        <Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        <Nav className="mr-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#features">Features</Nav.Link>
+      <Nav.Link href="#pricing">Pricing</Nav.Link>
+        </Nav>
+    </Navbar>
+
                 <h1>HomeTest</h1>
+                <h1>SHOP</h1>
+                <button>Test</button>
                 {currentUser ? (
                     <p>You are logged in - <Link to="/Dashboard">View teststuff</Link></p>
                 ) : (
