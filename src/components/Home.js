198cdb097661ba10f1b8e6e6eff8fcee92ee6cdf
Puthinart-S <boatkakkakmak@gmail.com>
2021-05-02 04:22:59 +0700

    เอาไปไอกร๊วก

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,26 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link,Route } from 'react-router-dom';
 import { useAuth } from './AuthContext';
-import button from 'react-bootstrap/Button'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button,Navbar,Nav,ButtonGroup,Carousel} from 'react-bootstrap';
 import logo from './bidoflogo.png';
 import './style.css';
+import { Toy, Elect, Food, Fasions, Automotive, Books } from './Category';
 
-const toy = () => (
-    <div>
-        <h2>TEST toys</h2>
-    </div>
-)
 
-const elec = () => (
-    <div>
-        <h2>TEST elec</h2>
-    </div>
-)
 
 const Home = () => {
     const { currentUser } = useAuth();
+
+    const [catChange, setCatChange] = useState(false);
+    const [catChange2, setCatChange2] = useState(false);
+    const [catChange3, setCatChange3] = useState(false);
+    const [catChange4, setCatChange4] = useState(false);
+    const [catChange5, setCatChange5] = useState(false);
+    const [catChange6, setCatChange6] = useState(false);
+     
+     
+    const catToy = (e) => {
+        e.preventDefault();
+        setCatChange(!catChange);
+
+    }
+    
+    const catElec = (e) => {
+        e.preventDefault();
+        setCatChange2(!catChange2);
+
+    }
+
+    const catFood = (e) => {
+        e.preventDefault();
+        setCatChange3(!catChange3);
+
+    }
+
+    const catFasion = (e) => {
+        e.preventDefault();
+        setCatChange4(!catChange4);
+
+    }
+
+    const catAutomotive = (e) => {
+        e.preventDefault();
+        setCatChange5(!catChange5);
+
+    }
+
+    const catBooks = (e) => {
+        e.preventDefault();
+        setCatChange6(!catChange6);
+
+    }
+    
 
     return (
         <React.Fragment>
@@ -57,29 +92,42 @@ const Home = () => {
             </Carousel>
             <h3 className="disco">DISCOVER</h3>
             <h3 className="cat">CATAGORY</h3>
-            <div className="btncat">
-                <Button variant="secondary" size="lg" className="btnbox" onClick={toy}>
+            {(!catChange && !catChange2 && !catChange3 && !catChange4 && !catChange5 && !catChange6) ? 
+                (<div className="btncat">
+                <Button variant="secondary" size="lg" className="btnbox" onClick={catToy}>
                     TOYS
                 </Button>
-                <Button variant="secondary" size="lg" className="btnbox">
+                <Button variant="secondary" size="lg" className="btnbox" onClick={catElec}>
                     Electronic
                 </Button>
-                <Button variant="secondary" size="lg" className="btnbox">
+                <Button variant="secondary" size="lg" className="btnbox" onClick={catFood}>
                     Food & Beverage
                 </Button>
-                <Button variant="secondary" size="lg" className="btnbox">
+                <Button variant="secondary" size="lg" className="btnbox" onClick={catFasion}>
                     Fashion
                 </Button>
-                <Button variant="secondary" size="lg" className="btnbox">
+                <Button variant="secondary" size="lg" className="btnbox" onClick={catAutomotive}>
                     Automotive
                 </Button>
-                <Button variant="secondary" size="lg" className="btnbox">
+                <Button variant="secondary" size="lg" className="btnbox" onClick={catBooks}>
                     BOOKS
                 </Button>
-
-            </div>
+                </div>) :
+                (
+                <div className="btncat">
+                <Toy catChange={catChange} setCatChange={setCatChange}/>
+                <Elect catChange2={catChange2} setCatChange2={setCatChange2}/>
+                <Food catChange3={catChange3} setCatChange3={setCatChange3}/>
+                <Fasions catChange4={catChange4} setCatChange4={setCatChange4}/>
+                <Automotive catChange5={catChange5} setCatChange5={setCatChange5}/>
+                <Books catChange6={catChange6} setCatChange6={setCatChange6}/>
+                </div>
+                )
+                
+                
+            }
               
-                <Route path="/toy"><toy/></Route>
+                <Route path="/"></Route>
    
             
                 {currentUser ? (
