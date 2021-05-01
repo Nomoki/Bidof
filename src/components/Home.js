062fe5a2f5bfb30aef0eed20124bb7ae286497fa
Nomoki <sroravitoak@hotmail.com>
2021-05-01 07:29:55 +0700

    btn added
    bid offer btn added
    resize logo

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,35 +3,37 @@ import { Link } from 'react-router-dom';
 import { useAuth } from './AuthContext';
 import button from 'react-bootstrap/Button'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { Button,Navbar,Nav } from 'react-bootstrap';
+import { Button,Navbar,Nav,ButtonGroup} from 'react-bootstrap';
+import logo from './bidoflogo.png';
+import './style.css'
 
 const Home = () => {
     const { currentUser } = useAuth();
 
     return (
         <React.Fragment>
-            <Navbar bg="dark" variant="dark">
-        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+            <Navbar bg="light" variant="light">
+        <Navbar.Brand href="#home"><img src={logo} alt="Logo" className="logo" /></Navbar.Brand>
         <Nav className="mr-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
+      <Nav.Link href="#home">
+                        <Link to="/LogIn" className="btn btn-primary">Log In</Link>
+                    </Nav.Link>
+      <Nav.Link href="#features"><Link to="/SignUp" className="btn btn-success">Sign Up</Link></Nav.Link>
         </Nav>
     </Navbar>
-            <div className="container">
-        
-
-                <h1>HomeTest</h1>
-                <h1>SHOP</h1>
-                <button>Test</button>
+                <ButtonGroup aria-label="Basic example" className="topbtn">
+                    <Button variant="secondary">BID</Button>
+                    <Button variant="secondary">OFFER</Button>
+                </ButtonGroup>
+   
+            
                 {currentUser ? (
                     <p>You are logged in - <Link to="/Dashboard">View teststuff</Link></p>
                 ) : (
                     <p>
-                        <Link to="/LogIn" className="btn btn-primary">Log In</Link> or <Link to="/SignUp" className="btn btn-success">Sign Up</Link>
+                        
                     </p>
                 )}
-            </div>
         </React.Fragment>
     )
 }
