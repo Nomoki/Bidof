d8883a28cd5d0fa6c90fe9331805d424e06222ba
Puthinart-S <boatkakkakmak@gmail.com>
2021-05-02 23:14:04 +0700

    updath sth

--- src/components/Home.js
+++ src/components/Home.js
@@ -110,14 +110,15 @@ const Home = () => {
     return (
         <React.Fragment>
             <Navbar bg="light" variant="light">
-        <Navbar.Brand href="#home"><img src={logo} alt="Logo" className="logo" /></Navbar.Brand>
-        <Nav className="mr-auto">
-      <Nav.Link href="#home">
+                <Navbar.Brand href="#home"><img src={logo} alt="Logo" className="logo" /></Navbar.Brand>
+                <Nav className="mr-auto">
+                    <Nav.Link href="#home">
                         <Link to="/LogIn" className="btn btn-primary">Log In</Link>
                     </Nav.Link>
-      <Nav.Link href="#features"><Link to="/SignUp" className="btn btn-success">Sign Up</Link></Nav.Link>
-        </Nav>
-    </Navbar>
+                    <Nav.Link href="#features">
+                        <Link to="/SignUp" className="btn btn-success">Sign Up</Link></Nav.Link>
+                    </Nav>
+            </Navbar>
                 <ButtonGroup aria-label="Basic example" className="topbtn">
                     <Button variant="secondary">BID</Button>
                     <Button variant="secondary">OFFER</Button>
