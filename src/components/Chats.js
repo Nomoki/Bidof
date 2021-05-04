f0781fd84c8adea0f5a19129e08ef3b9d139a104
Fluke <43031042+h2oprince1@users.noreply.github.com>
2021-05-05 05:33:02 +0700

    Chats

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -1,11 +1,66 @@
-import React from 'react'
+import React from 'react';
+import './Chats.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+  useLocation
+} from "react-router-dom";
+import Login from './Loginch';
+import RoomList from './RoomList';
+import AddRoom from './AddRoom';
+import ChatRoom from './ChatRoom';
 
-const Chat = ()  => {
-    return (
-        <div>
-            
-        </div>
-    )
+function Chats() {
+  let location = useLocation();
+
+  return (
+    <Router>
+      <div>
+        <Redirect
+          to={{
+            pathname: "/roomlist",
+            state: { from: location }
+          }}
+        />
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <SecureRoute path="/roomlist">
+            <RoomList />
+          </SecureRoute>
+          <SecureRoute path="/addroom">
+            <AddRoom />
+          </SecureRoute>
+          <SecureRoute path="/chatroom/:room">
+            <ChatRoom />
+          </SecureRoute>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
-export default Chat
+export default Chats;
+
+function SecureRoute({ children, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={({ location }) =>
+        localStorage.getItem('nickname') ? (
+          children
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/login",
+              state: { from: location }
+            }}
+          />
+        )
+      }
+    />
+  );
+}
