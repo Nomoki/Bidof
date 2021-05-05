3bd4278856873c951ff880c67aac656d1fe8fc35
Nomoki <sroravitoak@hotmail.com>
2021-05-05 10:58:38 +0700

    Update CreateOfferPosts.js

--- src/components/CreateOfferPosts.js
+++ src/components/CreateOfferPosts.js
@@ -156,7 +156,9 @@ const CreateOfferPost = () => {
                 </>) : (
                 <>
                 <form onSubmit={editProduct}>
+                <div className="selpicprobid">
                 <input type="file" onChange={onFileChange}/>
+                </div>
                 <div className="postbox">
                     <Form.Group >
                         <Form.Control ref={prodNameRef} className="proname" size="lg" type="text" placeholder="Product name" />
